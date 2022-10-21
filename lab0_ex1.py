26d991ac51b5989432a10d9261068123b0613259
KendiNoreen <kendikaburu57@gmail.com>
2022-10-21 17:44:22 +0300

    first trial

---
+++ lab0_ex1.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+print("Hello Again")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+def main():
+    print("Hello World!")
+    print("Hello Again")
+    print('Yay! Printing.')
+    print("I'd much rather you 'not'.")
+    print('I "said" do not touch this.')
+    print("I'd much rather you 'not'.")
+    print('I "said" do not touch this.')
+main()
---
+++ lab0_ex2.py
@@ -0,0 +1,30 @@
+import math
+
+def introduction_message():
+    print("This program prints the roots of a second order equation")
+    print("   ax^2 + bx + c = 0")
+    print()
+
+
+def input_and_solve():
+    a = int(input("please enter a: "))
+    b = int(input("please enter b: "))
+    c = int(input("please enter c: "))
+    delta = b*b - 4*a*c
+
+    x1 = -b + math.sqrt(delta)
+    x2 = -b - math.sqrt(delta)
+
+    print("The Roots are:")
+    print("x1 = ", x1)
+    print("x2 = ", x2)
+
+
+def final_message():
+    print("Thank you for using this Program...")
+    print("----------------------------------")
+    print("All Rights Preserved.")
+
+introduction_message()
+input_and_solve()
+final_message
